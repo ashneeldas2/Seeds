e9035ca7317fb9984e7957253c7d41f3605be7b7
Amanda Hittelman <amandahittelman@Amandas-MacBook-Pro.local>
2017-01-21 09:17:15 -0500

    Initial Commit

---
+++ Seeds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seeds
+//
+//  Created by Amanda Hittelman on 1/21/17.
+//  Copyright © 2017 Amanda Hittelman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
